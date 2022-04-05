e7f6c49b2e163bbb800cf7e72abb08b80c8801cf
Shubham Kashikar <54555834+skashika@users.noreply.github.com>
2022-04-05 12:14:55 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/input', methods=['POST'])
+def addOne():
+    new_quark = request.get_json()
+    print('Input from user: ', new_quark)
+    x = {"sum": new_quark["value1"] + new_quark["value2"]}
+    print('Output: ', x)
+    return x
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
