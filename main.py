f4df21ecfedcd1d9d9615e0bace59b981059d256
Shubham Kashikar <54555834+skashika@users.noreply.github.com>
2022-04-05 15:02:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,6 @@ def addOne():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
